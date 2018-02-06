2a4ffc7b706d308ec44bf27c8a2769212d34a78e
Harris <matthew.d.harris@aecom.com>
2018-02-06 12:07:15 -0500

    before velox conversion

--- R/model_mh.R
+++ R/model_mh.R
@@ -6,13 +6,24 @@ library("sp")
 library("mapview")
 library("FedData")
 library("elevatr")
-library("snow")    # Parallel
+library("doParallel")    # Parallel
+library("foreach")    # Parallel
 
 ## Data locations
 data_loc <- "U:/z_OldServer/Projects/NY/Oakdale_Fayette_model/GIS"
 SHP_loc  <- file.path(data_loc,"SHP","Client_files")
 
-## ignored folders
+output_res_ft = 50
+use_parallel  = FALSE
+
+if(isTRUE(use_parallel)){
+  # http://www.gis-blog.com/increasing-the-speed-of-raster-processing-with-r-part-23-parallelisation/
+  #Define how many cores you want to use
+  UseCores <- detectCores() -1
+  cl       <- makeCluster(UseCores)
+  registerDoParallel(cl)
+}
+
 if(!dir.exists("RASTERS")){
   dir.create("RASTERS")
 }
@@ -30,8 +41,8 @@ clSP       <- read_sf(file.path(SHP_loc, "ProposedFinalRouteCL_10-05-2017.shp"))
 clSP_sp    <- as(clSP, "Spatial")
 temp_bboxSP<- read_sf(file.path(data_loc,"SHP","Temp","temp_bbox.shp"))
 
-m1 <- mapview(list(sitesSP, clSP, temp_bboxSP))
 
+start.time <- Sys.time()
 
 ### buffering
 cl_5mi_buffSP  <- st_buffer(st_combine(clSP), 26400) %>%
@@ -49,54 +60,44 @@ cl_525mi_buffSP  <- st_buffer(st_combine(clSP), 27720) %>%
 cl_525mi_buffSP  <- st_intersection(cl_525mi_buffSP, temp_bboxSP)  #### <- CROPPED FOR TESTING!!!!!!!
 cl_525mi_buffSP_sp <- as(cl_5mi_buffSP, "Spatial")
 
-beginCluster(8) ###### <- PARALLEL n=cores!!
-
 #### RASTERS!!!! FTW!
 ### get Elevation data & crop
 elevation_full <- get_elev_raster(cl_5mi_buffWGS_sp, z = 12, src = "aws")
 elevation <- mask(elevation_full, cl_5mi_buffWGS_sp)
-elevationSP <- projectRaster(elevation, crs = CRS(SPNYCentral83), res = 50)
-# writeRaster(elevationSP,file.path("RASTERS","elevationSP.tiff"))
-m1 <- mapview(elevationSP)
-m2 <- mapview(sitesSP)
-# m1 + m2
+elevationSP <- projectRaster(elevation, crs = CRS(SPNYCentral83),
+                             res = output_res_ft) 
+writeRaster(elevationSP,file.path("RASTERS","elevationSP2.tiff"))
 
 # slope
 slope <- raster::terrain(elevation, out = "slope", unit='radians')
 slope <- tan(slope)*100
 slopeSP <- projectRaster(slope, elevationSP)
-# writeRaster(slopeSP,file.path("RASTERS","slop_degree.tiff"))
-m3 <- mapview(slopeSP)
-# m2 + m3
+writeRaster(slopeSP,file.path("RASTERS","slop_degree2.tiff"))
 
 ### NED 
-NHD <- FedData::get_nhd(template=cl_525mi_buffWGS_sp, label='test')
+NHD <- FedData::get_nhd(template=cl_525mi_buffWGS_sp, label='test') ### <- change for full run
 # flowlines
 flowlines <- NHD[["_Flowline"]]
 flowlinesSP <- spTransform(flowlines, SPNYCentral83)
 flowlinesSP_r <- rasterize(flowlinesSP, elevationSP)
 flowlinesSP_r[flowlinesSP_r > 0] <- 1
-# writeRaster(flowlinesSP_r,file.path("RASTERS","flowlines.tiff"), overwrite=TRUE)
-m4 <- mapview(flowlinesSP_r)
+writeRaster(flowlinesSP_r,file.path("RASTERS","flowlines2.tiff"), overwrite=TRUE)
 flowlinesSP_dist <- raster::distance(flowlinesSP_r)
-flowlinesSP_dist <- projectRaster(flowlinesSP_dist, elevationSP)
+# flowlinesSP_dist <- projectRaster(flowlinesSP_dist, elevationSP) # not needed
 flowlinesSP_dist <- mask(flowlinesSP_dist, elevationSP)
-# writeRaster(flowlinesSP_dist,file.path("RASTERS","fflowlinesSP_dist.tiff"), overwrite=TRUE)
-m5 <- mapview(flowlinesSP_dist)
+writeRaster(flowlinesSP_dist,file.path("RASTERS","fflowlinesSP_dist2.tiff"), overwrite=TRUE)
 
 ## AREA (large streams)
 areas <- NHD[["_Area"]]
 m6_a <- mapview(areas)
 areasSP <- spTransform(areas, SPNYCentral83)
-areasSP_r <- rasterize(areasSP, elevationSP)
+areasSP_r <- rasterize(areasSP, elevationSP, progress="text")
 areasSP_r[areasSP_r > 0] <- 1
-# writeRaster(areasSP_r,file.path("RASTERS","areasSP.tiff"), overwrite=TRUE)
-m6 <- mapview(areasSP_r)
+writeRaster(areasSP_r,file.path("RASTERS","areasSP2.tiff"), overwrite=TRUE)
 areasSP_dist <- raster::distance(areasSP_r)
-areasSP_dist <- projectRaster(areasSP_dist, elevationSP)
+# areasSP_dist <- projectRaster(areasSP_dist, elevationSP) # not needed
 areasSP_dist <- mask(areasSP_dist, elevationSP)
-# writeRaster(areasSP_dist,file.path("RASTERS","areasSP_dist.tiff"), overwrite=TRUE)
-m7 <- mapview(areasSP_dist)
+writeRaster(areasSP_dist,file.path("RASTERS","areasSP_dist2.tiff"), overwrite=TRUE)
 
 ## Waterbodies
 waterbody <- NHD[["_Waterbody"]]
@@ -104,14 +105,15 @@ m8_a <- mapview(waterbody)
 waterbodySP <- spTransform(waterbody, SPNYCentral83)
 waterbodySP_r <- rasterize(waterbodySP, elevationSP)
 waterbodySP_r[waterbodySP_r > 0] <- 1
-writeRaster(waterbodySP_r,file.path("RASTERS","waterbodySP.tiff"), overwrite=TRUE)
-m8 <- mapview(waterbodySP_r)
+writeRaster(waterbodySP_r,file.path("RASTERS","waterbodySP2.tiff"), overwrite=TRUE)
 waterbodySP_dist <- raster::distance(waterbodySP_r)
-waterbodySP_dist <- projectRaster(waterbodySP_dist, elevationSP)
+# waterbodySP_dist <- projectRaster(waterbodySP_dist, elevationSP) # not needed
 waterbodySP_dist <- mask(waterbodySP_dist, elevationSP)
-# writeRaster(waterbodySP_dist,file.path("RASTERS","waterbodySP_dist.tiff"), overwrite=TRUE)
-m9 <- mapview(waterbodySP_dist)
+writeRaster(waterbodySP_dist,file.path("RASTERS","waterbodySP_dist2.tiff"), overwrite=TRUE)
 
 ### take min of H20 dists for min to H20
 
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
 
